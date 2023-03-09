aff652c08b56e06909f0f6b0585497e2e5bfc8de
Fabio Maia <fabiomaia@ciandt.com>
2023-03-09 11:12:48 -0300

    feat: [CIT-113] creating module for mig.

---
+++ modules/mig/variables.tf
@@ -0,0 +1,35 @@
+variable "project_id" {
+  description = ""
+  type        = string 
+  default     = "" 
+}
+
+variable "region" {
+  description = ""
+  type        = string
+  default     = "" 
+}
+
+variable "network_name" {
+  description = ""
+  type        = string
+  default     = "" 
+}
+
+variable "subnet_name" {
+  description = ""
+  type        = string
+  default     = "" 
+}
+
+variable "subnet_ip" {
+  description = ""
+  type        = string 
+  default     = "" 
+}
+
+variable "subnet_region" {
+  description = ""
+  type        = string 
+  default     = ""
+}
