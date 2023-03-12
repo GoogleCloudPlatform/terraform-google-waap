9c90ee0d5fc2edde8dc59b90a16fbd427a4ceb69
Fabio Maia <fabiomaia@ciandt.com>
2023-03-12 14:28:32 -0300

    [fix] Fix syntax and fix errors lint.

--- modules/mig/main.tf
+++ modules/mig/main.tf
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /*****************
 *** Create VPC ***
 ******************/
@@ -44,11 +60,10 @@ resource "google_compute_firewall" "inbound-ip-ssh" {
   direction = "INGRESS"
   allow {
     protocol = "tcp"
-    ports    = ["22", "8080"] # SSH port and Jenkins port  
+    ports    = ["22", "8080"] # SSH port and Jenkins port
   }
   source_ranges = [
     "35.235.240.0/20"
   ]
   source_tags = ["allow-ssh-iap"]
-
 }
