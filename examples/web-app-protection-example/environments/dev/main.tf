71ec2b45c9fdf1f6d7c2fd55012e6ece8b7cc6bd
Fabio Maia <fabiomaia@ciandt.com>
2023-03-14 16:46:38 -0300

    [CIT-107]delete env

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -2,13 +2,13 @@ locals {
   environment = "dev"
 }
 
-module "base_mig" {
-    source = "../../../../modules/mig"
+# module "base_mig" {
+#     source = "../../../../modules/mig"
 
-    project_id    = var.project_id
-    region        = var.region
-    network_name  = var.network_name
-    subnet_name   = var.subnet_name
-    subnet_ip     = var.subnet_ip
-    subnet_region = var.subnet_region
-}
+#     project_id    = var.project_id
+#     region        = var.region
+#     network_name  = var.network_name
+#     subnet_name   = var.subnet_name
+#     subnet_ip     = var.subnet_ip
+#     subnet_region = var.subnet_region
+# }
