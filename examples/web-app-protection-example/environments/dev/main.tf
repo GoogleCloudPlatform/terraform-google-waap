cb68e92ad69da0fc956d696c414af12b6bc6a337
Fabio Maia <fabiomaia@ciandt.com>
2023-03-16 15:24:30 -0300

    [CIT-107]Creating VPC R1 and R2

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -1,14 +1,40 @@
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 locals {
   environment = "dev"
 }
 
-# module "base_mig" {
-#     source = "../../../../modules/mig"
+module "network_mig_r1" {
+    source = "../../../../modules/mig"
 
-#     project_id    = var.project_id
-#     region        = var.region
-#     network_name  = var.network_name
-#     subnet_name   = var.subnet_name
-#     subnet_ip     = var.subnet_ip
-#     subnet_region = var.subnet_region
-# }
+    project_id    = var.project_id
+    region        = var.region_r1
+    network_name  = var.network_name_r1
+    subnet_name   = var.subnet_name_r1
+    subnet_ip     = var.subnet_ip_r1
+    subnet_region = var.subnet_region_r1
+}
+
+module "network_mig_r2" {
+    source = "../../../../modules/mig"
+
+    project_id    = var.project_id
+    region        = var.region_r2
+    network_name  = var.network_name_r2
+    subnet_name   = var.subnet_name_r2
+    subnet_ip     = var.subnet_ip_r2
+    subnet_region = var.subnet_region_r2
+}
--- examples/web-app-protection-example/environments/dev/terraform.tfvars
+++ examples/web-app-protection-example/environments/dev/terraform.tfvars
@@ -1,8 +1,7 @@
 /***********************
 *** Shared Variables ***
 ************************/
-project_id   = "cicd-prj-bbee"
-
+project_id   = "ci-waap-caba"
 
 
 /*************************
@@ -19,8 +18,8 @@ subnet_region_r1 = "us-central1"
 *** Region 2 Variables ***
 **************************/
 
-region_r2        = "us-central1"
+region_r2        = "us-east1"
 network_name_r2  = "webapp-r2"  # prefix vpc
 subnet_name_r2   = "webapp-r2"  # prefix subnet
 subnet_ip_r2     = "10.0.32.0/24"
-subnet_region_r2 = "us-central1"
+subnet_region_r2 = "us-east1"
