9c63a7f7626ae26de26d86d9183205492c79a531
Matheus Buitrago <mbuitrago@ciandt.com>
2023-03-20 16:13:57 -0300

    [CIT-77] run build for lb test

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -107,4 +107,84 @@ module "mig_r2" {
    
 }
 
-## Backend LB
+module "lb-http" {
+  source  = "GoogleCloudPlatform/lb-http/google"
+  version = "7.0.0"
+
+  name    = "lb-web-app"
+  project = var.project
+  target_tags = [
+    var.tags_r1,
+    var.tags_r2,
+  ]
+  firewall_networks = [module.network_mig_r1.network_name, module.network_mig_r2.network_name]
+
+  backends = {
+    default = {
+
+      description                     = null
+      protocol                        = "HTTP"
+      port                            = 80
+      port_name                       = "http"
+      timeout_sec                     = 10
+      connection_draining_timeout_sec = null
+      enable_cdn                      = false
+      security_policy                 = null
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+      custom_request_headers          = null
+      custom_response_headers         = null
+
+      health_check = {
+        check_interval_sec  = null
+        timeout_sec         = null
+        healthy_threshold   = null
+        unhealthy_threshold = null
+        request_path        = "/"
+        port                = 80
+        host                = null
+        logging             = null
+      }
+
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+
+      groups = [
+        {
+          group                        = module.mig_r1
+          balancing_mode               = null
+          capacity_scaler              = null
+          description                  = null
+          max_connections              = null
+          max_connections_per_instance = null
+          max_connections_per_endpoint = null
+          max_rate                     = null
+          max_rate_per_instance        = null
+          max_rate_per_endpoint        = null
+          max_utilization              = null
+        },
+        {
+          group                        = module.mig_r2
+          balancing_mode               = null
+          capacity_scaler              = null
+          description                  = null
+          max_connections              = null
+          max_connections_per_instance = null
+          max_connections_per_endpoint = null
+          max_rate                     = null
+          max_rate_per_instance        = null
+          max_rate_per_endpoint        = null
+          max_utilization              = null
+        },
+      ]
+
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = ""
+        oauth2_client_secret = ""
+      }
+    }
+  }
+}
