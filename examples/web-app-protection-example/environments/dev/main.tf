43c8785c6e2789ec323174efd96f3db618acdba6
Matheus Buitrago <mbuitrago@ciandt.com>
2023-03-20 16:40:12 -0300

    [CIT-77] fix value compression_mode

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -129,6 +129,7 @@ module "lb-http" {
       timeout_sec                     = 10
       connection_draining_timeout_sec = null
       enable_cdn                      = false
+      compression_mode                = null
       security_policy                 = null
       session_affinity                = null
       affinity_cookie_ttl_sec         = null
