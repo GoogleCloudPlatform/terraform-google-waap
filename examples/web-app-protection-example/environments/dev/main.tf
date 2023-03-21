aaa84212e9cb15536c83a8e8aaa36ae0379e53fe
Matheus Buitrago <mbuitrago@ciandt.com>
2023-03-21 15:56:58 -0300

    [CIT-77] run build for lb test

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -114,8 +114,7 @@ module "lb-http" {
   name    = "lb-web-app"
   project = var.project_id
   target_tags = [
-    var.tags_r1,
-    var.tags_r2,
+    "backend-r1", "backend-r2"
   ]
   firewall_networks = [module.network_mig_r1.network_name, module.network_mig_r2.network_name]
 
