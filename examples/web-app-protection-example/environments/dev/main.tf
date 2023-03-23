3a6927dc8f4734dfb19e38b6e580f7f763c2384b
Fabio Maia <fabiomaia@ciandt.com>
2023-03-23 16:39:16 -0300

    [fixes]Fixes lints and checks

--- examples/web-app-protection-example/environments/dev/main.tf
+++ examples/web-app-protection-example/environments/dev/main.tf
@@ -1,18 +1,30 @@
-locals {
-  environment = "dev"
-}
+/**
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 data "template_file" "startup_script" {
-  template  = <<EOT
+  template = <<EOT
     #!/bin/bash
     set -x
     curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
     sudo bash add-google-cloud-ops-agent-repo.sh --also-install
-    
+
     # Install docker and run the juice shop application.
     sudo apt-get -y install ca-certificates curl gnupg lsb-release
     sudo mkdir -m 0755 -p /etc/apt/keyrings
-    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg  
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update
@@ -23,101 +35,98 @@ data "template_file" "startup_script" {
 }
 
 module "network_mig_r1" {
-    source = "../../../../modules/mig_network"
+  source = "../../../../modules/mig_network"
 
-    project_id    = var.project_id
-    region        = var.region_r1
-    network_name  = var.network_name_r1
-    subnet_name   = var.subnet_name_r1
-    subnet_ip     = var.subnet_ip_r1
-    subnet_region = var.subnet_region_r1
+  project_id    = var.project_id
+  region        = var.region_r1
+  network_name  = var.network_name_r1
+  subnet_name   = var.subnet_name_r1
+  subnet_ip     = var.subnet_ip_r1
+  subnet_region = var.subnet_region_r1
 }
 
 module "network_mig_r2" {
-    source = "../../../../modules/mig_network"
+  source = "../../../../modules/mig_network"
 
-    project_id    = var.project_id
-    region        = var.region_r2
-    network_name  = var.network_name_r2
-    subnet_name   = var.subnet_name_r2
-    subnet_ip     = var.subnet_ip_r2
-    subnet_region = var.subnet_region_r2
+  project_id    = var.project_id
+  region        = var.region_r2
+  network_name  = var.network_name_r2
+  subnet_name   = var.subnet_name_r2
+  subnet_ip     = var.subnet_ip_r2
+  subnet_region = var.subnet_region_r2
 }
 
 module "mig_r1" {
   source = "../../../../modules/mig"
 
   # VM Template
-  project_id          = var.project_id
-  region              = var.region_r1
-  name_prefix         = var.name_prefix_r1
-  machine_type        = var.machine_type_r1
-  tags                = var.tags_r1
+  project_id   = var.project_id
+  region       = var.region_r1
+  name_prefix  = var.name_prefix_r1
+  machine_type = var.machine_type_r1
+  tags         = var.tags_r1
 
-  source_image        = var.source_image_r1
-  disk_size_gb        = var.disk_size_gb_r1
+  source_image = var.source_image_r1
+  disk_size_gb = var.disk_size_gb_r1
 
-  service_account     = var.service_account_id_r1
-  roles               = var.service_account_roles_r1
-  scopes              = var.service_account_scopes_r1
+  service_account = var.service_account_id_r1
+  roles           = var.service_account_roles_r1
+  scopes          = var.service_account_scopes_r1
 
-  startup_script      = "${data.template_file.startup_script.rendered}"
+  startup_script = data.template_file.startup_script.rendered
 
-  network             = var.network_name_r1
-  subnetwork          = var.subnet_name_r1
-  
+  network    = var.network_name_r1
+  subnetwork = var.subnet_name_r1
+
   # Managed Instance Group
-  mig_name            = var.mig_name_r1
-  base_instance_name  = var.base_instance_name_r1
-  zone                = var.zone_r1
+  mig_name           = var.mig_name_r1
+  base_instance_name = var.base_instance_name_r1
+  zone               = var.zone_r1
 
-  target_size         = var.target_size_r1
+  target_size = var.target_size_r1
 }
 
 module "mig_r2" {
   source = "../../../../modules/mig"
 
   # VM Template
-  project_id           = var.project_id
-  region               = var.region_r2
-  name_prefix          = var.name_prefix_r2
-  machine_type         = var.machine_type_r2
-  tags                 = var.tags_r2
+  project_id   = var.project_id
+  region       = var.region_r2
+  name_prefix  = var.name_prefix_r2
+  machine_type = var.machine_type_r2
+  tags         = var.tags_r2
 
-  source_image         = var.source_image_r2
-  disk_size_gb         = var.disk_size_gb_r2
+  source_image = var.source_image_r2
+  disk_size_gb = var.disk_size_gb_r2
 
-  service_account      = var.service_account_id_r2
-  roles                = var.service_account_roles_r2
-  scopes               = var.service_account_scopes_r2
+  service_account = var.service_account_id_r2
+  roles           = var.service_account_roles_r2
+  scopes          = var.service_account_scopes_r2
 
-  startup_script       = "${data.template_file.startup_script.rendered}"
+  startup_script = data.template_file.startup_script.rendered
 
-  network              = var.network_name_r2
-  subnetwork           = var.subnet_name_r2
-  
-  
+  network    = var.network_name_r2
+  subnetwork = var.subnet_name_r2
 
   # Managed Instance Group
-  mig_name             = var.mig_name_r2
-  base_instance_name   = var.base_instance_name_r2
-  zone                 = var.zone_r2
+  mig_name           = var.mig_name_r2
+  base_instance_name = var.base_instance_name_r2
+  zone               = var.zone_r2
 
-  target_size          = var.target_size_r2
-   
+  target_size = var.target_size_r2
 }
 
 module "lb-http" {
   source  = "GoogleCloudPlatform/lb-http/google"
   version = "7.0.0"
 
-  name    = "lb-web-app"
-  project = var.project_id
+  name        = "lb-web-app"
+  project     = var.project_id
   target_tags = ["backend-r1", "backend-r2"]
-  
+
   firewall_networks = [module.network_mig_r1.network_name, module.network_mig_r2.network_name]
   firewall_projects = var.firewall_projects
-  
+
   backends = {
     default = {
 
@@ -156,7 +165,7 @@ module "lb-http" {
         default_ttl = 3600
         client_ttl  = 1800
         max_ttl     = 28800
-        
+
         cache_key_policy = {
           include_host          = true
           include_protocol      = true
