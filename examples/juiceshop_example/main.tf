dd2912840a23f56ca95217f5494a5cd004c477ef
bharathkkb <bharathkrishnakb@gmail.com>
2023-12-18 21:24:06 +0000

    chore: update examples to use registry

--- examples/juiceshop_example/main.tf
+++ examples/juiceshop_example/main.tf
@@ -51,7 +51,8 @@ module "cloud_router" {
 }
 
 module "apigee" {
-  source = "../../modules/apigee"
+  source = "GoogleCloudPlatform/waap/google//modules/apigee"
+version="~> REPLACE-AFTER-RELEASE"
 
   project_id          = var.project_id
   create_apigee_org   = true
@@ -807,7 +808,8 @@ resource "google_compute_security_policy" "waap_policies" {
 
 // WAAP Analytics
 module "analytics" {
-  source = "../../modules/waap-analytics"
+  source = "GoogleCloudPlatform/waap/google//modules/waap-analytics"
+version="~> REPLACE-AFTER-RELEASE"
 
   project_id     = var.project_id
   log_sink_name  = "juiceshop_log_sink"
